7e875c32d73c239ecd0d3372abe4b8c5fef5aaee
DanielCG <danielcgilibert@gmail.com>
2023-02-20 17:44:20 +0100

    🚀 add features

--- app/auth/Logged.tsx
+++ app/auth/Logged.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { signOut } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -9,7 +10,9 @@ type User = {
 export default function Logged({ image }: User) {
   return (
     <li className="flex gap-8 items-center">
-      <button className="text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25">
+      <button
+        onClick={() => signOut()}
+        className="text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25">
         Sign Out
       </button>
       <Link href={'/dashboard'}>
